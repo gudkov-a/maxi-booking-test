3843bf27e9aa2e93f4460dd0c3407c3f3a35b047
Gudkov Alexey <gudkov.alexey.s@gmail.com>
2020-01-19 06:42:41 -0500

    add second view. create base HTML container

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_currency')
+def get_currency():
+    return 'currency info'
+
+
+if __name__ == '__main__':
+    app.config['DEBUG'] = True
+    app.run('0.0.0.0')
